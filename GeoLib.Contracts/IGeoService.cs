af48c60865367c87a61f5be310facdfbbd950199
Brighty28 <abright2012@gmail.com>
2016-06-30 10:09:42 +0100

    WCF end to end course
    Initial commit

---
+++ GeoLib.Contracts/IGeoService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Contracts
+{
+    [ServiceContract]
+    public interface IGeoService
+    {
+        [OperationContract]
+        ZipCodeData GetZipCodeData(string zip);
+
+        [OperationContract]
+        IEnumerable<string> GetStatesData(bool primaryOnly);
+
+        [OperationContract(Name = "GetZipsByState")]
+        IEnumerable<ZipCodeData> GetZips(string state);
+
+        [OperationContract(Name = "GetZipsForRange")]
+        IEnumerable<ZipCodeData> GetZips(string zip, int range);
+    }
+}
---
+++ GeoLib.Tests/ManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit;
+using GeoLib.Data;
+using GeoLib.Contracts;
+using GeoLib.Services;
+using FluentAssertions;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class ManagerTests
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "LINCOLN PARK",
+                State = new State() {Abbreviation = "NJ"},
+                Zip = "07035"
+            };
+
+            mockZipCodeRepository.Setup(obj => obj.GetByZip("07035")).Returns(zipCode);
+
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+
+            ZipCodeData data = geoService.GetZipCodeData("07035");
+
+            data.City.ToUpper().Should().Be("LINCOLN PARK");
+            data.State.Should().Be("NJ");
+        }
+    }
+}
